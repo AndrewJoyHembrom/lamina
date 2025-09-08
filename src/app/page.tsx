eea99b88214147263a8069e7d90872ab31224e7b
AndrewJoyHembrom <dumbdrewski2003@gmail.com>
2025-09-08 23:55:33 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 
-export default async function Home() {
-  return <Button>Click Me</Button>;
+// export default async function Home() {
+//   return <Button>Click Me</Button>;
+// }
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("/dashboard");
 }
