ae0bb916935df79acd4e3bd3005377980bef42df
AndrewJoyHembrom <dumbdrewski2003@gmail.com>
2025-10-22 20:34:14 +0530

    uplaod meetings task incomplete

--- src/app/(protected)/dashboard/meeting-card.tsx
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -1,10 +1,73 @@
 "use client";
 
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import { Card } from "@/components/ui/card";
+import { useDropzone } from "react-dropzone";
 import React from "react";
+import { uploadFile } from "@/lib/firebase";
+import { set } from "zod";
+import { is } from "date-fns/locale";
+import { Presentation, Upload } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const MeetingCard = () => {
+  const [isUploading, setIsUploading] = React.useState(false);
   const [progress, setProgress] = React.useState(0);
-  return <div></div>;
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {
+      "audio/*": [".mp3", ".wav", ".m4a"],
+    },
+    multiple: false,
+    maxSize: 50_000_000,
+    onDrop: async (acceptedFiles) => {
+      // Handle file upload and transcription here
+      setIsUploading(true);
+      console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      const downloadURL = await uploadFile(file as File, setProgress);
+      setIsUploading(false);
+    },
+  });
+  return (
+    <Card
+      className="col-span-2 flex flex-col items-center justify-between p-10"
+      {...getRootProps()}
+    >
+      {!isUploading && (
+        <>
+          <Presentation className="h-10 w-10 animate-bounce" />
+          <h3 className="mt-2 text-sm font-semibold text-gray-900">
+            Create a new meeting
+          </h3>
+          <p className="mt-1 text-center text-sm text-gray-500">
+            Analyse your meeting with Lamina.
+            <br />
+            Powered by AI.
+          </p>
+          <div className="mt-6">
+            <Button disabled={isUploading}>
+              <Upload className="mr-1.5 -ml-0.5 h-5 w-5" aria-hidden="true" />
+              Upload Meeting
+              <input className="hidden" {...getInputProps()} />
+            </Button>
+          </div>
+        </>
+      )}
+
+      {isUploading && (
+        <div className="flex items-center justify-center">
+          <CircularProgressbar
+            value={progress}
+            text={`${progress}%`}
+            className="size-20"
+          />
+          <p className="text-center text-sm text-gray-500">
+            Uploading your meeting...
+          </p>
+        </div>
+      )}
+    </Card>
+  );
 };
 
 export default MeetingCard;
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage";
-import { get } from "http";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
